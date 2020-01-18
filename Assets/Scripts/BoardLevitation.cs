90151379dc08d82c1e044771e27efbfb78b0557c
Ryan <contact@ryanbrianjones.co.uk>
2020-01-18 18:09:15 +0000

    Convert to 3D project

--- Assets/Scripts/BoardLevitation.cs
+++ Assets/Scripts/BoardLevitation.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class BoardLevitation : MonoBehaviour
 {
     private float L0 = 1.0f;
-    private Rigidbody2D rb2D;
+    private Rigidbody rb;
+    private BoxCollider boxCollider;
     private float k = -88.68f;
     private float zeta = 0.3f;
+    public Rigidbody board;
+    private int springsInParallel = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        k /= springsInParallel;
     }
 
     // Update is called once per frame
@@ -28,23 +33,24 @@ public class BoardLevitation : MonoBehaviour
 
     void CalcForce(float L0)
     {
-        Vector3 adjustedPos = new Vector2(transform.position.x, transform.position.y - 0.4038926f / 2);
+        Vector3 adjustedPos = new Vector3(transform.position.x, transform.position.y - boxCollider.bounds.extents.y, transform.position.z);
 
         LayerMask mask = LayerMask.GetMask("Ground");
 
-        RaycastHit2D hit = Physics2D.Raycast(adjustedPos, -Vector2.up, 0.75f, mask);
+        Physics.Raycast(adjustedPos, -Vector3.up, out RaycastHit hit, 0.75f, mask);
         if (hit.collider != null)
         {
             float L = Mathf.Abs(hit.point.y - adjustedPos.y);
             float x = L0 - L;
 
-            float c = zeta * 2 * Mathf.Sqrt(rb2D.mass * -k);
-            Vector2 force = Vector3.up * (-k * x - c * rb2D.velocity.y);
+            float c = zeta * 2 * Mathf.Sqrt(board.mass * -k);
+            Vector2 force = Vector3.up * (-k * x - c * rb.velocity.y);
 
-            Debug.DrawRay(adjustedPos, -Vector2.up * L);
+            Debug.DrawRay(adjustedPos, -Vector3.up * L);
             Debug.Log("F = " + force.y + " L = " + L + " L0 = " + L0 + " x = " + x + " c = " + c);
 
-            rb2D.AddForce(force);
+            //rb.AddForce(force); to levitate the board directly upwards instead of in the direction of the thrusters
+            rb.AddRelativeForce(force);
         }
     }
 }
